b3182a47f7c6a85787b7a77643ce392a7ff62aba
aitorortega03 <aitorortega21@gmail.com>
2025-01-11 15:54:51 +0100

    Add controller feature

---
+++ src/main/java/com/aitorortegadev/auth_service/controller/AuthApi.java
@@ -0,0 +1,17 @@
+package com.aitorortegadev.auth_service.controller;
+
+import com.aitorortegadev.auth_service.common.dtos.TokenResponse;
+import com.aitorortegadev.auth_service.common.dtos.UserRequest;
+import com.aitorortegadev.auth_service.constants.ApiPathConstants;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.AUTH_ROUTE)
+public interface AuthApi {
+
+    @PostMapping(ApiPathConstants.REGISTER_ROUTE)
+    ResponseEntity<TokenResponse> createUser(@RequestBody @Valid UserRequest userRequest);
+}
---
+++ src/main/java/com/aitorortegadev/auth_service/controller/impl/AuthController.java
@@ -0,0 +1,23 @@
+package com.aitorortegadev.auth_service.controller.impl;
+
+import com.aitorortegadev.auth_service.common.dtos.TokenResponse;
+import com.aitorortegadev.auth_service.common.dtos.UserRequest;
+import com.aitorortegadev.auth_service.controller.AuthApi;
+import com.aitorortegadev.auth_service.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController implements AuthApi {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> createUser(UserRequest userRequest) {
+        return ResponseEntity.ok(authService.createUser(userRequest));
+    }
+}
