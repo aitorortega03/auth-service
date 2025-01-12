8243cdf919bdb8790d9a263f235624e53c5bfeaa
aitorortega03 <aitorortega21@gmail.com>
2025-01-12 15:39:16 +0100

    Add service logic

--- src/main/java/com/aitorortegadev/auth_service/service/impl/AuthServiceImpl.java
+++ src/main/java/com/aitorortegadev/auth_service/service/impl/AuthServiceImpl.java
@@ -2,10 +2,13 @@ package com.aitorortegadev.auth_service.service.impl;
 
 import com.aitorortegadev.auth_service.common.dtos.TokenResponse;
 import com.aitorortegadev.auth_service.common.dtos.UserRequest;
+import com.aitorortegadev.auth_service.common.mappers.UserMapper;
 import com.aitorortegadev.auth_service.repository.UserRepository;
 import com.aitorortegadev.auth_service.service.AuthService;
 import com.aitorortegadev.auth_service.service.JwtService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -14,13 +17,20 @@ public class AuthServiceImpl implements AuthService {
 
     private final JwtService jwtService;
 
-    public AuthServiceImpl(UserRepository userRepository, JwtService jwtService) {
+    private final UserMapper userMapper;
+
+    public AuthServiceImpl(UserRepository userRepository, JwtService jwtService, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.jwtService = jwtService;
+        this.userMapper = userMapper;
     }
 
     @Override
     public TokenResponse createUser(UserRequest userRequest) {
-        return null;
+        return Optional.of(userRequest)
+                .map(userMapper::toEntity)
+                .map(userRepository::save)
+                .map(userCreated -> jwtService.generateToken(userCreated.getId()))
+                .orElseThrow(() -> new RuntimeException("Error creating user"));
     }
 }
