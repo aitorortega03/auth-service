20f833d71bb1a8f301a34db9c2aac5bc2188e8cf
aitorortega03 <aitorortega21@gmail.com>
2025-01-12 15:37:16 +0100

    Add new method to user repository

--- src/main/java/com/aitorortegadev/auth_service/repository/UserRepository.java
+++ src/main/java/com/aitorortegadev/auth_service/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.aitorortegadev.auth_service.repository;
 import com.aitorortegadev.auth_service.common.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
